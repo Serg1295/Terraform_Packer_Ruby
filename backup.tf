7213193c4c923553adef7eed223d2c0af4cbc810
Sergo Hovakimyan <sergo.hovakimyan.12@gmail.com>
2022-01-13 16:50:48 +0400

    initial commit

--- backup.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sergo.tfstatebucket"
-    key    = "P161/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
--- varibles.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-  description = "Please enter region: " #---Default region is Francfurt ------
-  type        = string
-  default     = "eu-central-1"
-}
