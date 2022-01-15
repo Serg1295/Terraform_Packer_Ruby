a95e323c2db61160ffa57bc18d5dce117025b7ac
Sergo Hovakimyan <sergo.hovakimyan.12@gmail.com>
2022-01-15 20:48:15 +0400

    initial commit

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ sleep 30
 sudo apt-get update
 sudo apt-get install curl -y
 sudo apt install gnupg -y
+sudo apt install awscli -y
 curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
 curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg --import -
 curl -sSL https://get.rvm.io | sudo bash -s stable
@@ -11,5 +12,7 @@ rvm user all
 rvm requirements
 rvm install ruby-2.6.6
 rvm use 2.6.6 --default
+sudo apt-get install libmysqlclient-dev -y
+sudo apt install mysql-client -y
 bundle install
 sudo apt install nodejs -y
